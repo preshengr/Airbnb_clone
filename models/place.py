13070caee42312d341612f912efe6715c5d68c51
Precious <preshengr@gmail.com>
2024-03-25 12:20:58 +0100

    Place File updated

--- models/place.py
+++ models/place.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
-"""This module creates a Place class"""
+"""Defines the Place class."""
+
 
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """Class for managing place objects"""
+    """Place class for AirBnB project
+
+    city_id: string - empty string: it will be the City.id
+    user_id: string - empty string: it will be the User.id
+    name: string - empty string
+    description: string - empty string
+    number_rooms: integer - 0
+    number_bathrooms: integer - 0
+    max_guest: integer - 0
+    price_by_night: integer - 0
+    latitude: float - 0.0
+    longitude: float - 0.0
+    amenity_ids: list of string - empty list: becomes the list of Amenity.id
+    """
 
     city_id = ""
     user_id = ""
