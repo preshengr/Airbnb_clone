050931f942eaf1bf951145f3df687ed323414224
preshengr <preshengr@mail.com>
2024-05-20 10:21:54 +0100

    Code fix on amenity in models

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""This module creates a Amenity class"""
+"""Module base_model
+
+This Module contains a definition for Amenity Class
+"""
 
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Class for managing amenity objects"""
+    """A class that represents a amenity
+
+    Attribute:
+        name (str): the name of the amenity
+    """
 
     name = ""
