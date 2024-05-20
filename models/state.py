4669bf70b7c24927b4ed71697d522240e450be0e
preshengr <preshengr@mail.com>
2024-05-20 10:37:51 +0100

    code fix on state

--- models/state.py
+++ models/state.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""This module creates a User class"""
+"""Module base_model
+
+This Module contains a definition for State Class
+"""
 
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Class for managing state objects"""
+    """A class that represents a state
+
+    Attribute:
+        name (str): the name of the state
+    """
 
     name = ""
