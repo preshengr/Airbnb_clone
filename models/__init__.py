6f4ee714948d25871641d7f5038b352f796be36d
preshengr <preshengr@mail.com>
2024-05-20 10:23:00 +0100

    Code fix on init file models

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Initializes the package"""
+"""Creates a unique FileStorage instance for the application"""
+
 from models.engine.file_storage import FileStorage
+
+
 storage = FileStorage()
 storage.reload()
