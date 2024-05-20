3553060daf661a6ab968a6da16bc339980ef8066
preshengr <preshengr@mail.com>
2024-05-20 10:35:36 +0100

    Code fix on City

--- models/city.py
+++ models/city.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-"""This module creates a User class"""
+"""Module base_model
+
+This Module contains a definition for City Class
+"""
 
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Class for managing city objects"""
+    """A class that represents a city
+
+    Attributes:
+        name (str): name of the city
+        state_id (str): the state id
+    """
 
     state_id = ""
     name = ""
